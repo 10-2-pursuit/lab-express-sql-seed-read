eb24d74d9bbbb2e2c34bff2144de49e5b2ab308f
Shanice <shanicegriffin@pursuit.org>
2023-10-25 19:57:50 -0400

    fixed dbConfig file

--- back-end/queries/songs.js
+++ back-end/queries/songs.js
@@ -3,6 +3,7 @@ const db = require("../db/dbConfig.js");
 const getAllSongs = async () => {
     try {
         const allSongs = await db.any("SELECT * FROM songs");
+        return allSongs
     } catch(err) {
         return err
     }
