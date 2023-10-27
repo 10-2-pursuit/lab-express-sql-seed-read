47a9ec9601e9d4e04008ddee253f3e9b83a7e5cb
Shanice <shanicegriffin@pursuit.org>
2023-10-27 17:37:15 -0400

    added create and show one song

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllSongs } = require("../queries/songs.js");
+const { getAllSongs, createSong, getOneSong, deleteSong, updateSong } = require("../queries/songs.js");
 
 const songs = express.Router();
 
@@ -7,10 +7,30 @@ const songs = express.Router();
 songs.get("/", async (req, res) => {
     const allSongs = await getAllSongs();
     if (allSongs[0]) {
-        res.status(200).json({ success: true, data: { payload: allSongs } });
+        res.status(200).json(allSongs);
     } else {
-        res.status(500).json({ success: false, data: { error: "Server Error - we didn't do it!" } });
-    }
+        res.status(500).json({ success: false, data: { error: "Server Error - songs not retrieved!" } });
+    } 
 });
+
+songs.post("/", async (req, res) => {
+    try {
+        const createdSong = await createSong(req.body)
+        res.json(createdSong)
+    } catch (error) {
+        res.status(400).json({error: "There is an error"})
+    }
+})
+
+songs.get("/:id", async (req, res) => {
+    const {id} = req.params;
+    const oneSong = await getOneSong(id);
+     
+    if (oneSong) {
+        res.status(200).json(oneSong);
+    } else {
+        res.status(404).json({success: false, data:{ error: "Song not found!" }});
+    } 
+})
 
 module.exports = songs;
